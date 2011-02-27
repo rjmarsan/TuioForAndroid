356aaa683cb5b64eb00daafce1d28933ccec6b39
rj <rj@rjs-macbook.local>
2011-02-27 03:00:03 -0600

    Misc fixes, wondering how to sign app with system key

--- TUIOService/obj/local/armeabi-v7a/objs/helloneon/helloneon-intrinsics.o.d
+++
@@ -1,5 +0,0 @@
-/Users/rj/gitstuff/tuioandroid/TUIOService/obj/local/armeabi-v7a/objs/helloneon/helloneon-intrinsics.o: \
- /Users/rj/gitstuff/tuioandroid/TUIOService/jni/helloneon-intrinsics.c \
- /Users/rj/gitstuff/tuioandroid/TUIOService/jni/helloneon-intrinsics.h
-
-/Users/rj/gitstuff/tuioandroid/TUIOService/jni/helloneon-intrinsics.h:
--- TUIOService/obj/local/armeabi/objs/hello-jni/hello-jni..o.d
+++
@@ -1,8 +0,0 @@
-/Users/rj/gitstuff/tuioandroid/TUIOService/obj/local/armeabi/objs/hello-jni/hello-jni..o: \
- /Users/rj/gitstuff/tuioandroid/TUIOService/jni/hello-jni.cc \
- /Users/rj/android-ndk-r4-crystax/build/platforms/android-8/arch-arm/usr/include/jni.h \
- /Users/rj/android-ndk-r4-crystax/build/platforms/android-8/arch-arm/usr/include/android/log.h
-
-/Users/rj/android-ndk-r4-crystax/build/platforms/android-8/arch-arm/usr/include/jni.h:
-
-/Users/rj/android-ndk-r4-crystax/build/platforms/android-8/arch-arm/usr/include/android/log.h:
